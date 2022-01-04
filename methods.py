ad9a3feebc3b868afc2f28e2341c6bc3eceb46ac
Rahul Banerjee <rbanerjee28@gmail.com>
2022-01-04 03:27:42 +0100

    added Kalman Filtering

--- methods.py
+++ methods.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json
 import os
 from datetime import date
+from pykalman import KalmanFilter
 
 def read_config(filename):
     if (os.path.exists(filename)):
@@ -18,18 +19,24 @@ def get_data_from_web(tickers, source, start_date, end_date, latest_closing_date
     if latest_closing_date == True:
         today = date.today()
         end_date = today.strftime("%Y-%m-%d")
+        
     panel_data = web.DataReader(tickers, source, start_date, end_date)
+
     return panel_data
 
 
 def clean_data(raw_data, start_date, end_date, latest_closing_date):
     closing_price = raw_data['Close']
+
     if latest_closing_date == True:
         today = date.today()
         end_date = today.strftime("%Y-%m-%d")
+
     weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
     closing_price = closing_price.reindex(weekdays)
     closing_price = closing_price.fillna(method='ffill')
+    closing_price = closing_price.fillna(method='bfill')
+
     return closing_price
 
 
@@ -38,12 +45,25 @@ def plot_data(data, tickers):
         ticker_series = data.loc[:, ticker]
         short_term_rolling_average = ticker_series.rolling(window=7).mean()
         long_term_rolling_average = ticker_series.rolling(window=30).mean()
+        
+        kf = KalmanFilter(
+                transition_matrices=[1],
+                observation_matrices=[1],
+                initial_state_mean=0,
+                initial_state_covariance=1,
+                observation_covariance=1,
+                transition_covariance=0.01
+            )
+
+        state_means, _ = kf.filter(ticker_series)
+
 
         fig, ax = plt.subplots(figsize=(16,9))
 
         ax.plot(ticker_series.index, ticker_series, label=ticker)
         ax.plot(short_term_rolling_average.index, short_term_rolling_average, label='7 Days Rolling Average')
         ax.plot(long_term_rolling_average.index, long_term_rolling_average, label='30 Days Rolling Average')
+        ax.plot(ticker_series.index, state_means, label='Kalman Filter smoothed')
 
         ax.set_xlabel('Date')
         ax.set_ylabel('Adjusted closing price ($)')
